c4290455cd39081174c83aaba50cfbbe3396942e
gato <gato@chimuelo>
2018-03-10 15:44:40 -0600

    Arreglos de acceso y estilos para la base

--- sistema/nucleo/contenido-publico.php
+++ sistema/nucleo/contenido-publico.php
@@ -1,8 +1,11 @@
 <?php
-  include "./sistema/configuracion/sistema.php";
+    include "./sistema/configuracion/sistema.php";
 ?>
 <h1>
-  <?php echo $NOMBRE_DEL_SISTEMA ?>
+    <?php echo $NOMBRE_DEL_SISTEMA ?>
 </h1>
 
-&Eacute;ste contenido es de acceso por todos los visitantes.
+&Eacute;ste contenido es de acceso por todos los visitantes. <br />
+<a class="button" href="./?contenido=./sistema/nucleo/autenticacion.php">
+    Acceder
+</a>
--- sistema/nucleo/principal.php
+++ sistema/nucleo/principal.php
@@ -1,52 +1,51 @@
 <?php
     include "sistema/funciones/primarias.php";
 ?>
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>
-      <?php
-        echo "
-          $NOMBRE_DEL_SISTEMA
-        \n";
-      ?>
-    </title>
-    <?php
-      colocar_estilos();
-      colocar_guiones();
-    ?>
-  </head>
-  <body>
-    <div id='contenedor'>
-      <?php
-        evaluar_contenido();
-      ?>
-      <div id='encabezado'>
-        <center>
-          <h1>
+    <head>
+        <title>
             <?php
-              echo "
-                $NOMBRE_DEL_SISTEMA
-              \n";
+                echo "
+                    $NOMBRE_DEL_SISTEMA
+                \n";
             ?>
-          </h1>
-        </center>
+        </title>
         <?php
-          colocar_encabezado();
+            colocar_estilos();
+            colocar_guiones();
         ?>
-        <br/>
-      </div>
-      <div id='contenido'>
-        &nbsp;<br/>
-        <?php
-          colocar_contenido();
-        ?>
-      </div>
-      <div id='pedestal'>
-        &nbsp;<br/>
-        <?php
-          colocar_pedestal();
-        ?>
-      </div>
-    </div>
-  </body>
+    </head>
+    <body>
+        <div id='contenedor'>
+            <?php
+                evaluar_contenido();
+            ?>
+            <div id='encabezado'>
+                <h1>
+                    <?php
+                        echo "
+                            $NOMBRE_DEL_SISTEMA
+                        \n";
+                    ?>
+                </h1>
+                <?php
+                    colocar_encabezado();
+                ?>
+                <br/>
+            </div>
+            <div id='contenido'>
+                &nbsp;<br/>
+                <?php
+                    colocar_contenido();
+                ?>
+            </div>
+            <div id='pedestal'>
+                &nbsp;<br/>
+                <?php
+                    colocar_pedestal();
+                ?>
+            </div>
+        </div>
+    </body>
 </html>
